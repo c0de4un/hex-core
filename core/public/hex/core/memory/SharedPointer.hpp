3fdb3c270c4773bfd65b63100707c38b64f240bc
c0de4un <code4un@yandex.ru>
2023-05-08 17:10:10 +0400

    Implement SharedPointer logic

--- core/public/hex/core/memory/SharedPointer.hpp
+++ core/public/hex/core/memory/SharedPointer.hpp
@@ -8,14 +8,229 @@
  * SOFTWARE.
 **/
 
+#ifndef HEX_CORE_SHARED_POINTER_HPP
+#define HEX_CORE_SHARED_POINTER_HPP
+
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // INCLUDES
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// Include hex::core::MemoryManager
+#ifndef HEX_CORE_MEMORY_MANAGER_HPP
+    #include <hex/core/memory/MemoryManager.hpp>
+#endif /// !HEX_CORE_MEMORY_MANAGER_HPP
+
+// Include hex::core::IReferenceUser
+#ifndef HEX_CORE_I_REFERENCE_USER_HXX
+    #include <hex/core/memory/IReferenceUser.hxx>
+#endif /// !HEX_CORE_I_REFERENCE_USER_HXX
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//
+// TYPES
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+namespace hex
+{
+
+    namespace core
+    {
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // SharedPointer
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        template <typename T>
+        HEX_API class SharedPointer final : public IReferenceUser
+        {
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // META
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            HEX_CLASS
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        private:
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // FIELDS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            T* mAddress;
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // METHODS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            inline void reserve()
+            {
+                if (mAddress)
+                {
+                    hexMemory* const memoryManager(hexMemory::getInstance());
+
+                    if (memoryManager)
+                        memoryManager->addReferenceUser(static_cast<void*>(mAddress), static_cast<IReferenceUser*>(this));
+                }
+            }
+
+            inline void release() noexcept
+            {
+                if (mAddress)
+                {
+                    hexMemory* const memoryManager(hexMemory::getInstance());
+
+                    if (memoryManager)
+                    {
+                        try
+                        {
+                            memoryManager->removeReferenceUser(static_cast<void>(mAddress), static_cast<IReferenceUser*>(this));
+                        }
+                        catch (...)
+                        {
+                            // void
+                        }
+                    }
+                }
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // DELETED
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            SharedPointer(SharedPointer&&)            = delete;
+            SharedPointer& operator=(SharedPointer&&) = delete;
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        public:
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // CONSTRUCTORS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            explicit SharedPointer(T* pAddress = nullptr)
+                :
+                mAddress(pAddress)
+            {
+                reserve();
+            }
+
+            SharedPointer(const SharedPointer& other)
+                :
+                mAddress(other.mAddress)
+            {
+                reserve();
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // DESTRUCTOR
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            virtual ~SharedPointer() noexcept
+            {
+                release();
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // GETTERS & SETTERS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            T* get() noexcept
+            {
+                return mAddress;
+            }
+
+            long use_count() const noexcept
+            {
+                if (!mAddress)
+                    return 0;
+
+                try
+                {
+                    hexMemory* const memoryManager(hexMemory::getInstance());
+
+                    if (memoryManager)
+                        return memoryManager->getReferenceCount(mAddress);
+                }
+                catch (...)
+                {
+                    return 0;
+                }
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // OVERRIDE.IReferenceBlock
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            void onDeleteObject() final
+            {
+                T* address(mAddress);
+                mAddress = nullptr;
+
+                delete address;
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // METHODS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            void reset(T* pAddress = nullptr)
+            {
+                release();
+
+                mAddress = pAddress;
+
+                reserve();
+            }
+
+            bool equal(T* pAddress) const noexcept
+            {
+                return mAddress == pAddress;
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            // OPERATORS
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            T* operator->() noexcept
+            {
+                return mAddress;
+            }
+
+            void operator=(T* pAddress)
+            {
+                reset(pAddress);
+            }
+
+            bool operator==(T* pAddress) const noexcept
+            {
+                return equal(pAddress);
+            }
+
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        };
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    }
+
+}
+
+template <typename T>
+using hexShared = hex::core::SharedPointer<T>;
+
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#endif /// !HEX_CORE_SHARED_POINTER_HPP
